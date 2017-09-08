305a17ce3419eb5ce776f136960b2e8fc5c4bc2d
tfwoaini1234 <zxchobits@outlook.com>
2017-09-08 15:09:14 +0800

    one

---
+++ src/Tools/String/StringHandle.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yujigcheng
+ * Date: 2017/9/8
+ * Time: 14:47
+ */
+
+namespace Tools\String;
+
+
+class StringHandle
+{
+    public static function info(){
+        echo "此类主要对字符串格式等处理";
+    }
+}
