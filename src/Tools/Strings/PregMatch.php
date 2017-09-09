e2d38cd19c025dd689440200b86adf50f2e485fe
心 赵 <zhaoxinwork@outlook.com>
2017-09-10 00:17:12 +0800

    1.爬虫类 2.添加a标签匹配和img标签匹配

---
+++ src/Tools/Strings/PregMatch.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhaoxin
+ * Date: 2017/9/9
+ * Time: 23:21
+ */
+
+namespace Tools\Strings;
+
+
+class PregMatch
+{
+    /**
+     * 从字符串中匹配所有a标签中的href值
+     * @param $str
+     * @return array
+     */
+    public static function a($str)
+    {
+        if (!empty($str)) {
+            preg_match_all('/<a(.*?)href="(.*?)"(.*?)>(.*?)<\/a>/i', $str, $matches);
+            if (!empty($matches[2])) {
+                return $matches[2];
+            }
+            return array();
+
+        }
+        return array();
+    }
+
+    public static function img($str){
+        if(!empty($str)){
+            $num = preg_match_all('/<img(.*?)src="(.*?)"(.*?)\/>/i',$str,$matches);
+            if(!empty($matches[2])){
+                return $matches[2];
+            }
+            return array();
+        }
+        return array();
+    }
+
+}
