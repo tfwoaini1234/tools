1786ba82d1313eebcd98d15e4160642647725bfd
tfwoaini1234 <zhaoxin251721676@qq.com>
2018-11-11 20:14:40 +0800

    增加下载图片

--- src/Tools/Internet/HttpTools.php
+++ src/Tools/Internet/HttpTools.php
@@ -162,4 +162,73 @@ class HttpTools
         }
         return false;
     }
+
+
+
+    /** 下载远程图片保存到本地
+     * @param $url string 下载地址
+     * @param string $save_dir 保存路径
+     * @param string $filename 保存文件名 当保存文件名称为空时则使用远程文件原来的名称
+     * @param int $type
+     * @return array
+     */
+    function get_image($url, $save_dir = '', $filename = '', $type = 0){
+        if (trim($url) == '') {
+            return array(
+                'file_name' => '',
+                'save_path' => '',
+                'error' => 1
+            );
+        }
+        if (trim($save_dir) == '') {
+            $save_dir = './';
+        }
+        if (trim($filename) == '') { // 保存文件名
+            $ext = strrchr($url, '.');
+            if ($ext != '.gif' && $ext != '.jpg') {
+                return array(
+                    'file_name' => '',
+                    'save_path' => '',
+                    'error' => 3
+                );
+            }
+            $filename = time() . $ext;
+        }
+        if (0 !== strrpos($save_dir, '/')) {
+            $save_dir .= '/';
+        }
+        // 创建保存目录
+        if (! file_exists($save_dir) && ! mkdir($save_dir, 0777, true)) {
+            return array(
+                'file_name' => '',
+                'save_path' => '',
+                'error' => 5
+            );
+        }
+        // 获取远程文件所采用的方法
+        if ($type) {
+            $ch = curl_init();
+            $timeout = 5;
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, $timeout);
+            $img = curl_exec($ch);
+            curl_close($ch);
+        } else {
+            ob_start();
+            readfile($url);
+            $img = ob_get_contents();
+            ob_end_clean();
+        }
+        // 文件大小
+        $fp2 = @fopen($save_dir . $filename, 'a');
+        fwrite($fp2, $img);
+        fclose($fp2);
+        unset($img, $url);
+        return array(
+            'file_name' => $filename,
+            'save_path' => $save_dir . $filename,
+            'error' => 0
+        );
+    }
 }
