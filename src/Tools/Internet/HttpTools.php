8d5a587e3cc8a4deac9bbace944c8f00e39e4d74
tfwoaini1234 <zhaoxin251721676@qq.com>
2018-11-11 20:08:00 +0800

    增加post/get请求

--- src/Tools/Internet/HttpTools.php
+++ src/Tools/Internet/HttpTools.php
@@ -9,6 +9,9 @@
 namespace Tools\Internet;
 
 
+
+use Exception;
+
 class HttpTools
 {
     /**
@@ -40,11 +43,62 @@ class HttpTools
 
     }
 
+    /**
+     * 判断url格式是否正确
+     * @param $url
+     * @return bool
+     */
     public static function isUrl($url){
         $url = ltrim($url,'http://');
         if(!preg_match('/[\w.]+[\w\/]*[\w.]*\??[\w=&\+\%]*/is',$url)){
             return false;
         }
         return true;
+    }
+
+    /**
+     * post/get 合并请求
+     * @param $url string 请求链接
+     * @param $params array 请求参数
+     * @param string $method 请求方式
+     * @param array $header 请求头
+     * @param bool $multi 是否传输文件
+     * @return mixed
+     * @throws Exception
+     */
+    public static function do_request($url, $params=[], $method = 'GET', $header = array(), $multi = false) {
+        $opts = array(
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_RETURNTRANSFER => 1,
+            CURLOPT_SSL_VERIFYPEER => false,
+            CURLOPT_SSL_VERIFYHOST => false,
+            CURLOPT_HTTPHEADER => $header
+        );
+        /* 根据请求类型设置特定参数 */
+        switch(strtoupper($method)) {
+            //发送GET方法
+            case 'GET' :
+                $opts[CURLOPT_URL] = $url . '?' . http_build_query($params);
+                break;
+            case 'POST' :
+                //判断是否传输文件
+                $params = $multi ? $params : http_build_query($params);
+                $opts[CURLOPT_URL] = $url;
+                $opts[CURLOPT_POST] = 1;
+                $opts[CURLOPT_POSTFIELDS] = $params;
+                break;
+            default :
+                throw new Exception('不支持的请求方式！');
+        }
+        /**初始化并执行curl请求**/
+        $ch = curl_init();
+        curl_setopt_array($ch, $opts);
+        $data = curl_exec($ch);
+        $error = curl_error($ch);
+        curl_close($ch);
+        if ($error){
+            throw new Exception('请求发生错误：' . $error);
+        }
+        return $data;
     }
 }
